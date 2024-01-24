96a3c3d928ac14b3d76b8ac6a607b9843f63f85d
ZALOFARG <andre.work.go@gmail.com>
2024-01-23 19:43:07 -0800

    1st commit t1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""module that defines a function that concatenates"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """function that takes two args(str) and concatenates them"""
+    return str1 + str2
