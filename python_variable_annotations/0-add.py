94d83ed8a9063e0f0720ada297527eabc63d7b4d
ZALOFARG <andre.work.go@gmail.com>
2024-01-23 19:45:38 -0800

    feat. #!... -> #!...python3

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 """module that defines an add function"""
 
 
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """module that defines a function that concatenates"""
 
 
