11cd6df1fd6d249f02af407ff1ac3aaa62d1525c
ZALOFARG <andre.work.go@gmail.com>
2024-01-24 08:17:37 -0800

    feat. correct definition of "a"

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """module in which we'll define and describe different entities"""
 
-a:int = 1
+a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
 school: str = 'Holberton'
