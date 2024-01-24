70f6fcf45d7e99072f761e715b2839d1c59bf343
ZALOFARG <andre.work.go@gmail.com>
2024-01-23 19:25:48 -0800

    1st commit t0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+"""module that defines an add function"""
+
+
+def add(a: float, b: float) -> float:
+    """funciton that takes two args(float) and return their sum"""
+    return a + b
