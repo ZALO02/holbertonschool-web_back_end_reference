a0c97293032bb0570e68cafc062f6d752ad9c9f8
ZALOFARG <andre.work.go@gmail.com>
2024-01-23 19:52:24 -0800

    1st commit t2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module that defines a function that returns a floor of a float"""
+
+
+def floor(n: float) -> int:
+    """function that takes one arg(float) and return its floor"""
+    return int(n)
