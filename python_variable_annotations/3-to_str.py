c0f820d5fc154e4c99762ea91ee13b5e0e97adf7
ZALOFARG <andre.work.go@gmail.com>
2024-01-23 19:59:07 -0800

    1st commit t3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module that defines a function that returns a string representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """function that takes one arg(float) and return its string representation"""
+    return str(n)
