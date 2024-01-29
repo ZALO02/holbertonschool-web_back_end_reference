18573d0c515b2bfcd5f9385cb3a4ef7a3a4f3396
ZALOFARG <andre.work.go@gmail.com>
2024-01-29 08:26:39 -0800

    feat. variable annotations

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@
 import asyncio
 import random
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay: int = 10):
     """func takes an arg, waits for a rand delay and returns it
 
     Args:
@@ -14,7 +14,7 @@ async def wait_random(max_delay = 10):
         float: random number waited
 
     """
-    random_num = random.uniform(0, max_delay)
+    random_num: float = random.uniform(0, max_delay)
     await asyncio.sleep(random_num)
     return random_num
 
