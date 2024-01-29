a4b672f2bf8aba4039a7906e9b39c4ab031abcd3
ZALOFARG <andre.work.go@gmail.com>
2024-01-29 08:20:32 -0800

    1st commit of t0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""module that will work with the asyncio and random modules"""
+
+import asyncio
+import random
+
+async def wait_random(max_delay = 10):
+    """func takes an arg, waits for a rand delay and returns it
+
+    Args:
+        max_delay(int): number to wait
+
+    Returns:
+        float: random number waited
+
+    """
+    random_num = random.uniform(0, max_delay)
+    await asyncio.sleep(random_num)
+    return random_num
+
+
+if __name__ == '__main__':
+    asyncio.run(wait_random())
