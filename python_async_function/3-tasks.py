a95d385d32c94fef2435eb84a83bda5db22a9237
ZALOFARG <andre.work.go@gmail.com>
2024-02-02 19:39:14 -0800

    1st commit t3

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""module that will work with the asyncio and random modules"""
+
+import asyncio
+import time
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int = 10) -> asyncio.Task:
+    """func that takes and int and returns an asyncio task
+
+    Args:
+        max_delay: self-explanatory
+
+    Returns:
+        an asyncio task
+    """
+ 
+    task_1 = asyncio.create_task(wait_random(max_delay))
+    return task_1
