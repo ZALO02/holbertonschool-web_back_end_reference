113a920321e6757d0c0b1e14daef5eb6a1473016
ZALOFARG <andre.work.go@gmail.com>
2024-02-05 12:08:13 -0800

    fix: page size returned

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -45,11 +45,12 @@ class Server:
         """
         total_pages = math.ceil(len(self.dataset())/page_size)
         p = {
-                "page size": page_size,
+                "page size": len(self.get_page(page, page_size)),
                 "page": page,
                 "data": self.get_page(page, page_size),
                 "next page": page + 1 if page < total_pages else None,
-                "prev_page": page - 1 if page > 1 else None
+                "prev_page": page - 1 if page > 1 else None,
+                "total_pages": total_pages
         }
         return p
 
