5b43d20ba902dce596bbc2e3c446920c396ab9ee
andre <andre.work.go@gmail.com>
2024-02-13 14:16:46 -0500

    new: 1st commit t2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,57 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new Error('name is not a string')
+    }
+    this._name = name;
+
+    if (typeof length !== 'number') {
+      throw new Error('length is not a number')
+    }
+    this._length = length;
+
+    if (
+      !Array.isArray(students) ||
+      !students.every((student) => typeof student === 'string')
+      ) {
+      throw new Error('students must be an array of strings')
+    }
+    this._sudents = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name2) {
+    if (typeof name2 !== 'string') {
+      throw new Error('name is not a string')
+    }
+    this._name = name2;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length2) {
+    if (typeof length2 !== 'number') {
+      throw new Error('length is not a number')
+    }
+    this._length = length2;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students2) {
+    if (
+      !Array.isArray(students2) ||
+      !students2.every((student) => typeof student === 'string')
+      ) {
+      throw new Error('students must be an array of strings')
+    }
+    this._students = students2;
+  }
+}
