8df30646766e8d12f14baeec166b9477f7fa9f6a
ZALOFARG <andre.work.go@gmail.com>
2024-02-27 16:52:26 -0500

    fix: correct output

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -4,7 +4,7 @@ export default function getListStudents() {
       id: 1, firstName: "Guillaume", location: "San Francisco"
     },
     {
-      id: 2, firstName: "James", location: "San Columbia"
+      id: 2, firstName: "James", location: "Columbia"
     },
     {
       id: 5, firstName: "Serena", location: "San Francisco"
