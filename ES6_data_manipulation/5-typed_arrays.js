822ce53b6a8b23c8abb26ca096c60e7b3681ddcf
ZALOFARG <andre.work.go@gmail.com>
2024-02-28 12:45:15 -0500

    fix: capital letter of msg

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,5 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position > length || position < 0) throw new Error('position outside range');
+  if (position > length || position < 0) throw new Error('Position outside range');
 
   const arr = new Int8Array(length);
   arr[position] = value;
